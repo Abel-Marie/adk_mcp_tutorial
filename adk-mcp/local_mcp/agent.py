bb90b655ee841d58fd9776cf13076e286704fafb
Abel Marie <abelmarie49@gmail.com>
2025-12-03 16:42:32 +0300

    feat: add DB MCP client agent
    - create LlmAgent configured with gemini-2.5-flash-lite  model and DB_MCP_PROMPT
    - dynamically resolve absolute path to local server.py for stdio execution
    - integrate MCPToolset with StdioServerParameters to connect to SQLite MCP server

---
+++ adk-mcp/local_mcp/agent.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from google.adk.agents import LlmAgent
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParameters
+
+from local_mcp.prompt import DB_MCP_PROMPT
+
+PATH_TO_YOUR_MCP_SERVER_SCRIPT = str((Path(__file__).parent / "server.py").resolve())
+
+
+root_agent = LlmAgent(
+    model="gemini-2.5-flash-lite",
+    name="db_mcp_client_agent",
+    instruction=DB_MCP_PROMPT,
+    tools=[
+        MCPToolset(
+            connection_params=StdioServerParameters(
+                command="python3",
+                args=[PATH_TO_YOUR_MCP_SERVER_SCRIPT],
+            )
+        )
+    ],
+)
